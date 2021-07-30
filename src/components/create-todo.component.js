13d075136cb19acc35c9163204ea82f053a7c475
Mia Allen <mia@vidops.io>
2021-07-30 15:43:55 +0100

    Add radio buttons to create page

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -73,6 +73,49 @@ export default class CreateTodo extends Component {
                               onChange={this.onChangeToDoDescription}
                               />
                    </div>
+                   <div className="form-group">
+                       <label>Responsible: </label>
+                       <input type="text"
+                              className="form-control" 
+                              value={this.state.todo_responsible}
+                              onChange={this.onChangeToDoResponsible}
+                              />
+                   </div>
+                   <div className="form-group">
+                       <div  className="form-check form-check-inline">
+                           <input className="form-check-input"
+                                  type="radio"
+                                  name="priorityOptions"
+                                  id="priorityLow"
+                                  value="Low"
+                                  checked={this.state.todo_priority==='Low'}
+                                  onChange={this.onChangeToDoPriority}
+                                  />
+                            <label className="form-check-label">Low</label>       
+                       </div>
+                       <div  className="form-check form-check-inline">
+                           <input className="form-check-input"
+                                  type="radio"
+                                  name="priorityOptions"
+                                  id="priorityMedium"
+                                  value="Medium"
+                                  checked={this.state.todo_priority==='Medium'}
+                                  onChange={this.onChangeToDoPriority}
+                                  />
+                            <label className="form-check-label">Medium</label>       
+                       </div>
+                       <div  className="form-check form-check-inline">
+                           <input className="form-check-input"
+                                  type="radio"
+                                  name="priorityOptions"
+                                  id="priorityHigh"
+                                  value="High"
+                                  checked={this.state.todo_priority==='High'}
+                                  onChange={this.onChangeToDoPriority}
+                                  />
+                            <label className="form-check-label">High</label>       
+                       </div>
+                   </div>
                </form>
             </div>
         )
