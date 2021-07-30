238c351097b2b43b9520f40b02f55d48134301b5
Mia Allen <mia@vidops.io>
2021-07-30 14:00:04 +0100

    Added initial components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+
 class App extends Component {
   render() {
     return (
       <Router>
       <div className="container">
         <h2>MERN-Stack Todo App</h2>
-      </div>
-      <Route path="/" exact component={TodosList} />
-      <Route path="/edit/:id" component={EditTodo} />
-      <Route path="/create" component={CreateTodo} /> 
+        <Route path="/" exact component={TodosList} />
+        <Route path="/edit/:id" component={EditTodo} />
+        <Route path="/create" component={CreateTodo} />
+      </div> 
       </Router>
     );
   }
