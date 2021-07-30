a7db5ebc15ecc07ce22845868757765ee1e0f03b
Mia Allen <mia@vidops.io>
2021-07-30 15:53:36 +0100

    finished part 1 of tutorial

--- src/components/create-todo.component.js
+++ src/components/create-todo.component.js
@@ -116,6 +116,9 @@ export default class CreateTodo extends Component {
                             <label className="form-check-label">High</label>       
                        </div>
                    </div>
+                   <div className="form-group">
+                       <input type="submit" value="Create Todo" className="btn btn-primary" />
+                   </div>
                </form>
             </div>
         )
